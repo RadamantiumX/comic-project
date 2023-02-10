455bfcb6eb2f2a9f6758551687605da7f473814b
loadEdar <eduardo.arede@gmail.com>
2023-02-10 12:04:29 -0300

    Start project

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header (){
+
+    return(
+       <div className="header" id="start">
+           <div className="contenedor head">
+             
+           </div>
+       </div>
+    )
+}
+export default Header;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NavBar(){
+    return(
+        <nav>
+            <a href="">Soy Daniel</a>
+            <a href="">Eventos</a>
+            <a href="">Galeria</a>
+            <a href=""></a>
+        </nav>
+    )
+}
+export default NavBar;
